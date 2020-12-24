602f38f213b0b0c3cbe37b5d520cb417e92ab0a2
Thierry <37625040+ThierrryScotto@users.noreply.github.com>
2020-12-23 21:12:30 -0300

    Revert "Feature/create sqs class"

--- receive-grade-lambda/src/handler.js
+++ receive-grade-lambda/src/handler.js
@@ -3,19 +3,50 @@
 // environment variables
 require('dotenv').config();
 
-// sqs
-const sqs = require('./services/sqs'); 
+// dependencies
+const AWS = require('aws-sdk');
+
+var sqs = new AWS.SQS({ apiVersion: process.env.AWS_API_VERSION });
+
+const QueueUrl = process.env.AWS_SQS_QUEUE_URL;
 
 module.exports.receiveGrade = async (event, context) => {
-  const messages = await sqs.receiveMessage();
+  var params = {
+    AttributeNames: [
+       "SentTimestamp"
+    ],
+    MaxNumberOfMessages: 10,
+    MessageAttributeNames: [
+       "All"
+    ],
+    QueueUrl: QueueUrl,
+    VisibilityTimeout: 1,
+    WaitTimeSeconds: 0
+  };
 
-  if (!messages) {
-    return { statusCode: 404, body: JSON.stringify({ message: "The queue is empty" })};
-  }
+  sqs.receiveMessage(params, function(err, data) {
+    if (err) {
+      console.log("Receive Error", err);
+      return { statusCode: 400, body: JSON.stringify({ message: 'Error to get grades' })};
+    }
 
-  // toda a tratativa do sqs no dynamo
-
-  const messageDeleted = await sqs.deleteMessage(messages);
-
-  return { statusCode: 200, body: JSON.stringify(messageDeleted)};
+    if (data.Messages) {
+      var deleteParams = {
+        QueueUrl: QueueUrl,
+        ReceiptHandle: data.Messages[0].ReceiptHandle
+      };
+  
+      sqs.deleteMessage(deleteParams, function(err, data) {
+        if (err) {
+          console.log("Delete Error", err);
+          return { statusCode: 400, body: JSON.stringify({ message: `Error to delete grade ${data.ResponseMetadata.RequestId}` })};
+        } else {
+          console.log("Message Deleted", data);
+          return { statusCode: 200, body: JSON.stringify({ message: `Grade ${data.ResponseMetadata.RequestId} deleted from the queue` })};
+        }
+      });
+    }
+    console.log('Empty queue');
+    return { statusCode: 404, body: JSON.stringify({ message: `Empty QUEUE FIFO` })};
+  });
 };
